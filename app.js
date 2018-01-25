cac70467058688f2fb483db85c403790567bfcc6
Adhuron <pllana.a@gmail.com>
2018-01-25 10:29:11 +0100

    Read, Update and Delete of todos

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ app.set('views', __dirname + '/views');
 //Set tamplete engine
 app.set('view engine', "ejs");
 
+//body parser middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
 //Setup MongoDB
 const MongoClient = require('mongodb').MongoClient;
 const MongoURL= "mongodb://localhost:27017/todolist";
+const ObjectId= require ('mongodb').ObjectId;
 
 //Conecting to MongoDB
 MongoClient.connect(MongoURL, function(error, db) {
@@ -28,23 +36,88 @@ MongoClient.connect(MongoURL, function(error, db) {
 
 //Routes
 app.get('/', function(req, res) {
-	res.render ("index", {name: "Faqja fillestare", title: "Index"});
+	
+		todos.find().toArray(function(error, docs) {
+		if (error) {
+			console.log(error);
+		}	else {
+		res.render ("index", {docs: docs});		
+		}
+		});
+
+	
 });
 app.get ('/todos/:id', function(req, res) {
-	res.render("Show")
+	var id = ObjectId(req.params.id);
+	todos.findOne({_id: id}, function(error, doc) {
+		if(error) {
+			console.log(error);
+		}else {
+			res.render("Show", {doc: doc});
+		}
+	});
+
+	
 });
 app.post ('/todos/add', function(req, res) {
-	res.redirect ("/")
+	todos.insert({ title: req.body.title, description: req.body.description }, function(error, result) {
+		if (error) {
+			console.log(error);
+		}else {
+		res.redirect ("/")	
+		}
+	} );
+
+
+	
 });
-app.get ('/todos/edit/:id', function(req, res) {
-	res.render("edit")
+
+app.get('/todos/edit/:id', function(req, res) {
+	var id = ObjectId(req.params.id);
+	todos.findOne({_id: id}, function(error, doc) {
+
+		if(error) {
+			console.log(error);
+		} else {
+			res.render("edit", {doc: doc})
+		}
+	});
 });
+
+
 app.post('/todos/update/:id', function(req, res)
 {
-	res.redirect("/");
+	var id = ObjectId(req.params.id);
+	todos.updateOne({_id: id}, 
+	{$set: {title: req.body.title, description: req.body.description}}, function(error, result) {
+		if (error) {
+			console.log(error);
+		}else {
+		res.redirect("/");	
+		}
+	});
+
+	
 });
+
+
+
 app.get ('/todos/delete/:id', function(req, res) {
-	res.redirect("/");
+var id = ObjectId(req.params.id);
+
+todos.deleteOne({_id: id}, function(error, result) {
+	if(error) {
+		console.log(error);
+	}else {
+		res.redirect("/");
+	}
+} )
+
+
+	
+
+
+
 });
 
 					
